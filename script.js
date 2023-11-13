7c65eabf3af69acf8eeed39e8df1c3936c4c5a94
Chiara Puleio <chiarapuleio@yahoo.it>
2023-11-13 12:54:45 +0100

    js WIP

---
+++ script.js
@@ -0,0 +1,16 @@
+// window.onload = () => {
+//   fetch("https://deezerdevs-deezer.p.rapidapi.com/playlist/115", {
+//     headers: {
+//       "X-RapidAPI-Key": "896303ca42msh72d44ba7c276bc9p18b3ebjsna034926b180e",
+//       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+//     },
+//   })
+//     .then((response) => response.json)
+//     .then((playlistsObj) => {
+//       const playlistsArray = Object.values(playlistsObj);
+//       console.log(playlistsArray);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
