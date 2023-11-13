5f119752608ff611676170dfe0f1852eaf9034fe
Chiara Puleio <chiarapuleio@yahoo.it>
2023-11-13 14:11:19 +0100

    js - created fetch with random items

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-// window.onload = () => {
-//   fetch("https://deezerdevs-deezer.p.rapidapi.com/playlist/115", {
-//     headers: {
-//       "X-RapidAPI-Key": "896303ca42msh72d44ba7c276bc9p18b3ebjsna034926b180e",
-//       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-//     },
-//   })
-//     .then((response) => response.json)
-//     .then((playlistsObj) => {
-//       const playlistsArray = Object.values(playlistsObj);
-//       console.log(playlistsArray);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
+window.onload = () => {
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=podcasts", {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "896303ca42msh72d44ba7c276bc9p18b3ebjsna034926b180e",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      const list = document.getElementById("playlistList");
+      for (let i = 0; i < data.data.length; i++) {
+        const randomIndex = Math.floor(Math.random() * data.data.length);
+        const playlist = document.createElement("li");
+        playlist.innerText = data.data[randomIndex].title;
+
+        list.appendChild(playlist);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
