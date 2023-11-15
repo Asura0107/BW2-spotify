44a24ba1470a0378396f12cd41df3b70af9b6ee5
Chiara Puleio <chiarapuleio@yahoo.it>
2023-11-15 11:00:12 +0100

    corrections css and js

--- homepage.js
+++ homepage.js
@@ -4,8 +4,8 @@ window.onload = () => {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "896303ca42msh72d44ba7c276bc9p18b3ebjsna034926b180e",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
-    }
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
   })
     .then((response) => response.json())
     .then((data) => {
@@ -31,8 +31,8 @@ window.onload = () => {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "896303ca42msh72d44ba7c276bc9p18b3ebjsna034926b180e",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
-    }
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
   })
     .then((response) => response.json())
     .then((album) => {
@@ -64,16 +64,13 @@ window.onload = () => {
 
       const playBtn = document.createElement("button");
       playBtn.innerText = "Play";
-      playBtn.className =
-        "btn btn-success text-dark me-3 mb-3 rounded-5 play-album-btn fw-bold";
+      playBtn.className = "btn btn-success text-dark me-3 mb-3 rounded-5 play-album-btn fw-bold";
       const saveBtn = document.createElement("button");
       saveBtn.innerText = "Salva";
-      saveBtn.className =
-        "btn btn-outline-light me-3 mb-3 rounded-5 save-album-btn fw-bold";
+      saveBtn.className = "btn btn-outline-light me-3 mb-3 rounded-5 save-album-btn fw-bold";
       const moreBtn = document.createElement("button");
       moreBtn.innerText = ". . .";
-      moreBtn.className =
-        "btn btn-secondary mb-3 rounded-5 more-album-btn fw-bold";
+      moreBtn.className = "btn btn-secondary mb-3 rounded-5 more-album-btn fw-bold";
 
       const albumDetails = document.getElementById("albumDetails");
       const playerDiv = document.querySelector(".img-container");
@@ -176,7 +173,7 @@ window.onload = () => {
   const content = document.getElementById("albumContainer");
 
   hideButton.addEventListener("click", function () {
-    content.classList.add("d-none");
+    content.classList.add("d-lg-none");
   });
 
   //PLAYLIST
@@ -187,8 +184,8 @@ window.onload = () => {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "896303ca42msh72d44ba7c276bc9p18b3ebjsna034926b180e",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
-    }
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
   })
     .then((response) => response.json())
     .then((play) => {
@@ -230,24 +227,15 @@ window.onload = () => {
     });
 
   //CARD
-  const selectedcard = [
-    "jazz",
-    "pop",
-    "rock",
-    "afro",
-    "metal",
-    "blues",
-    "classic"
-  ];
+  const selectedcard = ["jazz", "pop", "rock", "afro", "metal", "blues", "classic"];
 
-  const randomcard =
-    selectedcard[Math.floor(Math.random() * selectedcard.length)];
+  const randomcard = selectedcard[Math.floor(Math.random() * selectedcard.length)];
   fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${randomcard}`, {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "896303ca42msh72d44ba7c276bc9p18b3ebjsna034926b180e",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
-    }
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
   })
     .then((response) => response.json())
     .then((play) => {
@@ -260,8 +248,7 @@ window.onload = () => {
         const row = document.querySelector(".row-card");
 
         const col = document.createElement("div");
-        col.className =
-          "col-sm-12 col-md-12 col-lg-3 g-1 justify-content-center";
+        col.className = "col-sm-12 col-md-12 col-lg-3 g-1 justify-content-center";
         row.appendChild(col);
 
         const divcard = document.createElement("div");
